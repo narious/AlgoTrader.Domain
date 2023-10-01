9a965d6330993f355bfaddbd3cc3bdd7c339cef8
Michael Dinh <tonkoalsce@gmail.com>
2023-10-01 15:16:17 +1100

    Setup basic database

--- Models/AssetTransaction.cs
+++ Models/AssetTransaction.cs
@@ -9,19 +9,11 @@ namespace AlgoTrader.Domain.Models
     public class AssetTransaction
     {
         public int Id { get; set; }
-        public DateTime Date { get; set; }
+        public DateTime DateProcessed { get; set; }
         public Account Account { get; set; }
         public Stock Stock { get; set; }
         public int Quantity { get; set; }
         public Currency_e Currency { get; set; }
 
-        public AssetTransaction(int id, DateTime date, Account account, Stock stock, int quantity)
-        {
-            Id = id;
-            Date = date;
-            Account = account;
-            Stock = stock;
-            Quantity = quantity;
-        }
     }
 }
